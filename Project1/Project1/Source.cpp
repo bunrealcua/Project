6589de70344edb59e754271d94a717bbfb612b0b
tonphuongoanh <oanh.phuongton@gmail.com>
2021-04-01 00:46:33 +0700

    Update Source.cpp

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
+	int a, b, c, d;
+	cout << "insert a,b,c,d " << endl;
+	cin >> a >> b >> c >> d;
 	return 0;
 }
 
-void Cfalskdfjieuygoiefhwiefheiutrwuaet
+- void Cfalskdfjieuygoiefhwiefheiutrwuaet
