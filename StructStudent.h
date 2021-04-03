4fa7a97e8e790aa5ce3611e0d1f5c24e58257b22
bunrealcua <voduynhan.123@gmail.com>
2021-04-03 20:11:44 +0700

    Update StructStudent.h

--- StructStudent.h
+++ StructStudent.h
@@ -7,10 +7,10 @@ using namespace std;
 
 struct Student {
 	int ID; wstring First_Name;
-	wstring Last_Name; wstring Gender;
-	wstring DateOfBirth; int Social_ID;
-	wstring username; wstring password;
-	wstring Class; int SchoolYear; Student* pNext;
+	wstring Last_Name; string Gender;
+	string DateOfBirth; int Social_ID;
+	string username; string password;
+	string Class; int SchoolYear; Student* pNext;
 };
 typedef struct Student Student;
 #endif
