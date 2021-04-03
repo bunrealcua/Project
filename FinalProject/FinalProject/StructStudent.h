808c00a9b689668516ea9b525bd63c64a56bae76
bunrealcua <voduynhan.123@gmail.com>
2021-04-03 16:01:46 +0700

    Update StructStudent.h

--- FinalProject/FinalProject/StructStudent.h
+++ FinalProject/FinalProject/StructStudent.h
@@ -7,8 +7,8 @@ using namespace std;
 
 struct Student {
 	int ID; wstring First_Name;
-	wstring Last_Name; wstring Gender;
-	wstring DateOfBirth; int Social_ID;
+	wstring Last_Name; wstring Gender; // Change type of data of Gender into bool, true:male and fale:female
+	wstring DateOfBirth; int Social_ID; // change type of data of DateOfBirth into int ddmmyyyy
 	wstring username; wstring password;
 	wstring Class; int SchoolYear; Student* pNext;
 };
