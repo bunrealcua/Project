199bb28d493cb1a6f2ee4680846c17f7c2f2020c
bunrealcua <voduynhan.123@gmail.com>
2021-04-07 15:52:30 +0700

    Trash dont worry

--- FinalProject/FinalProject/StructStudent.h
+++ FinalProject/FinalProject/StructStudent.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef FinalProject
 #define FinalProject
 #include<iostream>
@@ -32,3 +33,24 @@ void Signup(Student*& pHead);
 // course
 void crAdd();
 #endif
+=======
+#ifndef FinalProject
+#define FinalProject
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+struct Student {
+	int ID; wstring First_Name;
+	wstring Last_Name; string Gender; 
+	string DateOfBirth; int Social_ID; 
+	string username; string password;
+	string Class; int SchoolYear; Student* pNext;
+};
+typedef struct Student Student;
+
+void Input(string path, wstring& str, Student*& pHead);
+void SetUp(wstring str, Student*& pHead);
+#endif
+>>>>>>> Stashed changes
