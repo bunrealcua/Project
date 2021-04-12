babb85417b149ee3e5a7054a36a14f36e32b6d78
St0nkMlem <81601436+St0nkMlem@users.noreply.github.com>
2021-04-12 21:13:32 +0700

    Register (completed)

--- KTLT_Project/KTLT_Project/StructStudent.h
+++ KTLT_Project/KTLT_Project/StructStudent.h
@@ -5,7 +5,7 @@
 #include<string>
 using namespace std;
 
-typedef struct Student {
+struct Student {
 	int ID; wstring First_Name;
 	wstring Last_Name; bool Gender;
 	string DateOfBirth; int Social_ID;
@@ -41,7 +41,7 @@ void SetUpInfoCourse(wstring& wstr, Course*& pHead);
 void changepass(Student*& pHead);
 
 //Register new courses
-void register();
+void registercourse(Student* pHead, Course* pHead1);
 #endif
 
 
