61855836587f91741993fbbef09b7fea9f7670a7
bunrealcua <voduynhan.123@gmail.com>
2021-04-16 21:58:41 +0700

    Update Registercourses

--- KTLT_Project/KTLT_Project/Registercourses.cpp
+++ KTLT_Project/KTLT_Project/Registercourses.cpp
@@ -13,7 +13,7 @@ void CopyCourse(Course* CourseA, Course* CourseB);
 void SetUpRegistration(int schoolyear, int semester, Student* headStudent, Course* headCourse);
 void UpdateCourseSelection(int schoolyear, int semester, Student* headStudent);
 void DeleteACourseSelection(int schoolyear, int semester, Student* headStudent);
-
+void SelectCourse(Course* pCourse, Course*& CourseSelect);
 
  void registercourse(Student* pHeadStudent, Course* pHeadCourse)
 
@@ -50,59 +50,11 @@ void DeleteACourseSelection(int schoolyear, int semester, Student* headStudent);
 	 wcout << L"List of the course in this semester. Please select your courses. (Maximum 5 courses,Minimum 1 course)." << endl;
 	 Course* pCourse = nullptr;
 	 ViewListCourse(schoolyear, semester, pCourse);
-	 int max = 0;
 
 	 /*Giả định mỗi sinh viên đều phải chọn ít nhất 1 course.*/
-	 /*Nhận thông tin từ bàn phím sinh viên nhập*/
-	 /*Có một nút _getch() để cho option khi đăng kí*/
-	 int button;
-	 Course* CourseSelect = nullptr, * CourseCur = nullptr, * CoursePos = nullptr;
-	 Course* ptemp = nullptr;
-	 int pos = 0; /*Thứ tự course.*/
-	 while (max <= 5) {
-		 max++; /*Kiểm tra số lượng course đăng ký*/
-		 wcout << L"Choose course number:";
-		 wcin >> pos;
-		 CoursePos = pCourse;
-		 if (pos != 1) for (int i = 2;i < pos - 1;i++) CoursePos = CoursePos->next;
-		 /*Xác định course cần phải select*/ ptemp = CoursePos->next;
-		 if (CourseSelect == nullptr)
-		 {
-			 CourseSelect = new Course;
-			 CopyCourse(ptemp, CourseSelect);
-			 CourseSelect->next = nullptr;
-			 CourseCur = CourseSelect;
-		 }
-		 else {
-			 CourseCur->next = new Course;
-			 CourseCur = CourseCur->next;
-			 CopyCourse(ptemp, CourseCur);
-			 CourseCur->next = nullptr;
-		 }
-		 wcout << L"Successfully!" << endl;
-		 /*Cần phải xác định lại update và xóa.*/
-		 wcout << L"Option: 1. Select next  2. Select again  3. Done registration"; /*Biến nhận option*/
-		 button = _getch();
-		 /* 1.49(ASCII) 2.50(ASCII) 3.51(ASCII) */
-		 while (button != 49 && button != 50 && button != 51) button = _getch();
-		 if (button == 49) continue;
-		 if (button == 50)/*Chọn lại*/
-		 {
-			 /*Trường hợp chọn lại course đầu*/
-			 if (CourseCur == CourseSelect) { delete CourseSelect; CourseSelect = nullptr; }
-			 /*Trường hợp chọn lại course sau*/
-			 else {
-				 ptemp = CourseSelect;
-				 while (ptemp->next != CourseCur) ptemp = ptemp->next;
-				 delete CourseCur;
-				 ptemp->next = nullptr;
-				 CourseCur = ptemp;
-			 }
-		 }
-		 if (button == 51) break;
-		 /*Kiểm tra thời gian các course*/
-	 }
-	 /*Kiểm tra điều kiện, nếu 2 khóa học trùng thời gian học thì không được chọn*/ /*Tìm hiểu hàm thời gian*/
+	 Course* CourseCur = nullptr,*CourseSelect=nullptr;
+	 SelectCourse(pCourse, CourseSelect);/*Hàm chọn course của sinh viên.*/
+	 
 	 /*Sau khi đăng kí nên show ra list course đã đăng kí*/
 	 wcout << L"Here is the course you selected.";
 	 CourseCur = CourseSelect;
@@ -178,9 +130,117 @@ void DeleteACourseSelection(int schoolyear, int semester, Student* headStudent);
 /*Hàm để sinh viên thay đổi, update lựa chọn.*/
  void UpdateCourseSelection(int schoolyear, int semester, Student* headStudent)
  {
+	/*Hiển thị ra danh sách các course của staff nhập*/
+	 wcout << L"List courses in this semester.";
+	 Course* headCourse = nullptr;
+	 ViewListCourse(schoolyear, semester, headCourse);
+	/*Hiển thị danh sách các course sinh viên đã đăng ký. Hàm của Oanh. Xem và chỉnh sửa lại.*/
+	 wcout << L"Here your selected courses:" << endl;
 
+	/*Thay đổi lại dòng đã viết trong file Registration.txt.*/
+	 /*Xác định lại khóa học mới được update. Chuyển thành chuỗi string mới và Course ID mới. string.*/
+	 wcout << L"Please select your course again.";
+	 Course* CourseSelect = nullptr;
+	 /*Trong đó CourseSelect là linked list danh sách Course sinh viên đã chọn*/
+	 SelectCourse(headCourse, CourseSelect);
+
+	 /*Đọc string ra từ file. Và sửa lại. Xong rồi viết lại tất cả.*/
+	/*1.Biến Course ID và MSSV thành chuỗi string.*/
+	 string strtemp="";
+	 strtemp += to_string(headStudent->ID) + ',';
+	 Course* pCur = CourseSelect;
+	 while (pCur->next != nullptr) {
+		strtemp += to_string(pCur->ID) + ',';
+		pCur = pCur->next;
+	 }
+	 strtemp += to_string(pCur->ID);
+
+	 /*2.Lần lượt đọc ra và ghi lại vào trong file. Nếu gặp file mà có MSSV đầu thì thay thế.*/
+	 ifstream FileIn;
+	 ofstream FileOut;
+
+	 string str;/*string để đọc ra  từng chuỗi*/
+	 string strID="";/*Để lấy MSSV của mỗi string*/
+
+	 if (FileIn.is_open() == false || FileOut.is_open() == false) wcout << L"File registration can not be opened." << endl; else
+	 {
+		 while (FileIn>>str){
+			 for (int i = 0;str[i] != ',';i++) strID += str[i];
+			 if (strID == to_string(headStudent->ID)) str = strtemp;
+			 FileOut << str << endl;
+		 }
+	 }
+
+	 FileIn.close();
+	 FileOut.close();
+
+
+	/*Xóa linked list Course*/
+	 DeleteListCourse(CourseSelect);
+	 DeleteListCourse(headCourse);
  }
+
 /*Hàm để sinh viên xóa 1 course.*/
 void DeleteACourseSelection(int schoolyear, int semester, Student*headStudent)
-{}
+{
+
+}
+
+/*Hàm nhận thông tin từ sinh viên nhập bàn phím khi đăng ký course.*/
+/*Trong đó pCourse là linked list danh sách khóa học staff nhập*/
+/*Trong đó CourseSelect là linked list danh sách do sinh viên đăng ký.*/
+void SelectCourse(Course* pCourse, Course*&CourseSelect)
+{
+	/*Nhận thông tin từ bàn phím sinh viên nhập*/
+	 /*Có một nút _getch() để cho option khi đăng kí*/
+	int button;
+	Course * CourseCur = nullptr, * CoursePos = nullptr;
+	Course* ptemp = nullptr;
+	int pos = 0; /*Thứ tự course.*/
+	int max = 0;
+	while (max <= 5) {
+		max++; /*Kiểm tra số lượng course đăng ký*/
+		wcout << L"Choose course number:";
+		wcin >> pos;
+		CoursePos = pCourse;
+		if (pos != 1) for (int i = 2;i < pos - 1;i++) CoursePos = CoursePos->next;
+		/*Xác định course cần phải select*/ ptemp = CoursePos->next;
+		if (CourseSelect == nullptr)
+		{
+			CourseSelect = new Course;
+			CopyCourse(ptemp, CourseSelect);
+			CourseSelect->next = nullptr;
+			CourseCur = CourseSelect;
+		}
+		else {
+			CourseCur->next = new Course;
+			CourseCur = CourseCur->next;
+			CopyCourse(ptemp, CourseCur);
+			CourseCur->next = nullptr;
+		}
+		wcout << L"Successfully!" << endl;
+		/*Cần phải xác định lại update và xóa.*/
+		wcout << L"Option: 1. Select next  2. Select again  3. Done registration"; /*Biến nhận option*/
+		button = _getch();
+		/* 1.49(ASCII) 2.50(ASCII) 3.51(ASCII) */
+		while (button != 49 && button != 50 && button != 51) button = _getch();
+		if (button == 49) continue;
+		if (button == 50)/*Chọn lại*/
+		{
+			/*Trường hợp chọn lại course đầu*/
+			if (CourseCur == CourseSelect) { delete CourseSelect; CourseSelect = nullptr; }
+			/*Trường hợp chọn lại course sau*/
+			else {
+				ptemp = CourseSelect;
+				while (ptemp->next != CourseCur) ptemp = ptemp->next;
+				delete CourseCur;
+				ptemp->next = nullptr;
+				CourseCur = ptemp;
+			}
+		}
+		if (button == 51) break;
+		/*Kiểm tra thời gian các course*/
+	}
+	/*Kiểm tra điều kiện, nếu 2 khóa học trùng thời gian học thì không được chọn*/ /*Tìm hiểu hàm thời gian*/
+}
  
