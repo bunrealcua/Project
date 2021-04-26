57c7bd16d6796852f64951edba9f024870356f9f
Minh Châu <mzrharuji.c@gmail.com>
2021-04-26 11:32:14 +0700

    Update Main.cpp

--- KTLT_Project/KTLT_Project/Main.cpp
+++ KTLT_Project/KTLT_Project/Main.cpp
@@ -2,6 +2,7 @@
 #include"Time.h"
 #include"Staff.h"
 #include"Student.h"
+#include"visual.h"
 #include<iostream>
 #include<io.h>
 #include<fcntl.h>
@@ -19,6 +20,7 @@ using namespace std;
 struct Session { tm time; };
 int wmain()
 {
+<<<<<<< Updated upstream
 	
 
 	string path = "D:\\InputProject\\StaffAccount.txt";
@@ -125,5 +127,61 @@ int wmain()
 	
 
 
+=======
+	SetConsoleOutputCP(65001);
+	newScreen();
+	SetConsoleTitle(L"HCMUS - Course Registration System");
+	void DisableSelection();
+	/*Student* pHead;
+	Signup(pHead);*/
+	// Hàm này để thêm course, view course.
+	int schoolyear;
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	wcout << L"Semester 1 is coming. Input course in semester 1." << endl;
+	wcout << L"School year: "; wcin >> schoolyear;
+	short tempI;
+	wstring wtemp = L"", wstr = L"";
+
+	// Nhập các course.
+	CourseInfo* pHead = nullptr, * pCur = nullptr;
+	do {
+		wstr = L"";
+		wcout << L"Course ID: "; wcin >> tempI;
+		wstr += to_wstring(tempI) + L',';
+		wcout << L"Course name: "; wcin.ignore(); getline(wcin, wtemp);
+		wstr += wtemp + L",";
+		wcout << L"Lecturer: "; fflush(stdin); getline(wcin, wtemp);
+		wstr += wtemp + L",";
+		wcout << L"Number of credits: "; wcin >> tempI;
+		wstr += to_wstring(tempI) + L",";
+		wcout << L"Student limit: "; wcin >> tempI;
+		wstr += to_wstring(tempI) + L",";
+		wcout << L"Week day(1 for Sunday): "; wcin >> tempI;
+		wstr += to_wstring(tempI) + L",";
+		wcout << L"Session: "; wcin >> tempI;
+		wstr += to_wstring(tempI); // Check lại session sẽ nhập gì.
+		if (pHead == nullptr) {
+			pHead = new CourseInfo;
+			pHead->info = wstr;
+			pHead->next = nullptr;
+			pCur = pHead;
+		}
+		else {
+			pCur->next = new CourseInfo;
+			pCur = pCur->next;
+			pCur->info = wstr;
+			pCur->next = nullptr;
+		}
+		wcout << endl;
+		wcout << L"Add a new course.?? 1: Yes or 0: No" << endl;
+		tempI = _getch();
+
+	} while (tempI == 49);
+	SetUpSemester(1,1 ,pHead);
+	DeleteListCourseInfo(pHead);
+	wcout << L"Do you want to view the list of courses.";
+	tempI = _getch();
+>>>>>>> Stashed changes
 	return 0;
 }
